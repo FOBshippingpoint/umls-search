a6d6cd7ffcacddbc82c4a3a976140c7bd1c56396
rjun <rjun0729@gmail.com>
2023-05-09 06:27:46 +0800

    feat: done UMLS searching server

---
+++ server/src/test/java/com/sscs/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sscs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
